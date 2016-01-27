a16cb9cecaa8bb6886b6eb3160f47d1f5bb2bc54
gikundi <denisgikundi05@gmail.com>
2016-01-27 12:57:10 +0300

    initial commit

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+
+        <!-- start: Meta -->
+        <meta charset="utf-8">
+        <title>Growth Africa Interview </title>
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <meta name="keyword" content="">
+        <!-- end: Meta -->
+
+        <!-- start: Mobile Specific -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- end: Mobile Specific -->
+
+
+        <!-- start: CSS -->
+        <link id="bootstrap-style" href="{{asset('css/bootstrap.min.css')}}"rel="stylesheet">
+        <link href="{{asset('css/bootstrap-responsive.min.css')}}" rel="stylesheet">
+        <link id="base-style" href="{{asset('css/style.css')}}"rel="stylesheet">
+        <link id="base-style-responsive" href="{{asset('css/style-responsive.css')}}" rel="stylesheet">
+
+
+    </head>
+
+    <body>
+
+        @yield('content')
+
+
+    </body>
+</html>
